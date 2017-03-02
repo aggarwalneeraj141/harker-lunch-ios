cd6da0d6fd9636971c4218da065b2726307ff1b6
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2017-03-02 09:12:12 -0800

    init release

---
+++ harker-lunch-ios/harker-lunch-ios/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  harker-lunch-ios
+//
+//  Created by Neeraj Aggarwal on 3/2/17.
+//  Copyright © 2017 Neeraj Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var site: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //bound frame
+        self.site.frame = self.view.bounds
+        self.site.scalesPageToFit = true
+        //load page
+        let url = NSURL (string: "http://aggarwalneeraj141.github.io/harker-lunch/");
+        let requestObj = NSURLRequest(url: url! as URL);
+        site.loadRequest(requestObj as URLRequest);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
